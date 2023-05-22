d3390c144cf6ed0f0b5f2e8500c82c2dfb4e94d8
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2023-05-22 12:51:50 -0300

    added dead point

--- DeadPoint.cs
+++ DeadPoint.cs
@@ -6,5 +6,6 @@ public class DeadPoint : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D colission) {
         Debug.Log("dead" + colission.name);
+        Destroy(colission.gameObject);
     }
 }
