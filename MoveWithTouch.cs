5e7b395e5b8037bfce50afc0e1c1d8d8fcc50998
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2023-06-08 12:25:23 -0300

    fixed move with touch

--- MoveWithTouch.cs
+++ MoveWithTouch.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 public class MoveWithTouch : MonoBehaviour
 {
     [SerializeField] float speed = 0.4f;
-    // [SerializeField] float minX = 25.5F;
     [SerializeField] float startY = 5;
-    // [SerializeField] float maxX = 28.6F;
     [SerializeField] int lane;
     [SerializeField] float[] lanes = {25.2F, 26.9F, 28.6F};
 
@@ -24,23 +22,19 @@ public class MoveWithTouch : MonoBehaviour
 
     private void Update()
     {
-        // Touch touch = Input.GetTouch(0);
-        // if (touch.phase == TouchPhase.Began)
-        // {
-        //     if (touch.position.x < Screen.width / 2 && t.position.x < maxX)
-        //     {
-        //         if (lane != 0){
-        //             lane = lane-1;
-        //         }
-                
-        //     }
-        //     if (touch.position.x > Screen.width / 2 && t.position.x > minX)
-        //     {
-        //         if (lane != 2){
-        //             lane = lane+1;
-        //         }
-        //     }
-        // }
+        if (Input.touchCount < 0)
+        {Touch touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
+        {
+            if (touch.position.x < Screen.width / 2 && lane > 0)
+            {
+                    lane = lane-1;
+            }
+            if (touch.position.x > Screen.width / 2 && lane < 2)
+            {
+                    lane = lane+1;
+            }
+        }}
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             if (lane != 0){
