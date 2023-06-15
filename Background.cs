feec6e69cfc9e14df3903e9ee28a9989e3b5ee86
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2023-06-15 12:58:25 -0300

    line movement

--- Background.cs
+++ Background.cs
@@ -4,16 +4,19 @@ using System.Collections.Generic;
 
 public class Background : MonoBehaviour
 {
-    public float start = -5;
-    public float end = 10;
+    
+    [SerializeField] float speed = 0.04F;
+    public float start = 33.84f;
+    public float end = -8.66f;
     private Transform t;
 
-    private void Awake(){
+    private void Start(){
         t = GetComponent<Transform>();
     }
 
     private void Update() {
-        if (t.position.y >= end){
+        t.position = new Vector2(t.position.x, t.position.y - speed);
+        if (t.position.y <= end){
             t.position = new Vector3(t.position.x, start); 
         }
     }
