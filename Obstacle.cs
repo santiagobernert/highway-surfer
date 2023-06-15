703668a2732bee96541365bf166d055b5730d681
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2023-06-15 12:58:42 -0300

    spawn colission

--- Obstacle.cs
+++ Obstacle.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-
+    public bool first = false;
     public void OnCollisionEnter2D(Collision2D colission) {
         Debug.Log("choque");
     }
