ad183e44060dcbdb41b1f785c0941ae5c83e9f2a
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2023-06-26 12:44:28 -0300

    added coin and crash sound

--- Player.cs
+++ Player.cs
@@ -1,42 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.SceneManagement;
-using UnityEngine;
-using UnityEngine.UI;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine.SceneManagement;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class Player : MonoBehaviour
-{   
-    Score score;
-    Rigidbody2D rb;
-    public Text coinText;
-    int coins;
+    public class Player : MonoBehaviour
+    {   
+        [SerializeField] AudioSource coinAudio;
+        [SerializeField] AudioSource crashAudio;
+        Score score;
+        Rigidbody2D rb;
+        public Text coinText;
+        int coins;
 
-    public void OnCollisionEnter2D(Collision2D colission) {
-        if(score.scoreValue > score.highscore){
-            score.highscore = (int)score.scoreValue;
-            score.highscoreText.text = ""+score.highscore;
-            PlayerPrefs.SetInt("highscore", score.highscore);
+        public void OnCollisionEnter2D(Collision2D colission) {
+            crashAudio.Play();
+            if(score.scoreValue > score.highscore){
+                score.highscore = (int)score.scoreValue;
+                score.highscoreText.text = ""+score.highscore;
+                PlayerPrefs.SetInt("highscore", score.highscore);
+            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
 
-    public void OnTriggerEnter2D(Collider2D colission){
-        if(colission.gameObject.name.Contains("Coin")){
-            coins++;
-            PlayerPrefs.SetInt("coins", coins);
-            Destroy(colission.gameObject);
-            coinText.text = "" + coins;
+        public void OnTriggerEnter2D(Collider2D colission){
+            if(colission.gameObject.name.Contains("Coin")){
+                coins++;
+                PlayerPrefs.SetInt("coins", coins);
+                coinAudio.Play();
+                Destroy(colission.gameObject);
+                coinText.text = "" + coins;
+            }
         }
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        score = GameObject.Find("Score").GetComponent<Score>();
-        coinText = GameObject.Find("CoinsText").GetComponent<Text>();
-        coins = PlayerPrefs.GetInt("coins", 0);
-        // sm = GetComponent<SceneManager>()
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            score = GameObject.Find("Score").GetComponent<Score>();
+            coinText = GameObject.Find("CoinsText").GetComponent<Text>();
+            coins = PlayerPrefs.GetInt("coins", 0);
+            // sm = GetComponent<SceneManager>()
+        }
+        // Update is called once per frame
     }
-    // Update is called once per frame
-}
