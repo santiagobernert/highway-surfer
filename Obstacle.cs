fd3d88b0000df917de296e2b50e8ffb1c64da909
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2023-08-10 12:52:34 -0300

    change car skin randomly

--- Obstacle.cs
+++ Obstacle.cs
@@ -1,10 +1,22 @@
-using System;
 using UnityEngine;
+using System.Collections.Generic;
+using static UnityEngine.Random;
 
 public class Obstacle : MonoBehaviour
 {
     public bool first = false;
-    public void OnCollisionEnter2D(Collision2D colission) {
-        Debug.Log("choque");
+    public bool truck = false;
+    [SerializeField] List<Sprite> sprites;
+    private SpriteRenderer sr;
+    private Transform t;
+    // -4.3  -2.55  -0.7
+    
+
+    private void Start() {
+        t = GetComponent<Transform>();
+        sr = GetComponent<SpriteRenderer>();
+        if (truck == false){
+            sr.sprite = Instantiate(sprites[Random.Range(0, sprites.Count)]) as Sprite;
+        }
     }
 }
