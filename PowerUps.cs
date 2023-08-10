940bea91aa3fc4855b8f68b111832ee4b26bf880
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2023-08-10 12:52:25 -0300

    added powerups random

--- PowerUps.cs
+++ PowerUps.cs
@@ -1,13 +1,19 @@
-using System;
 using UnityEngine;
 using System.Collections.Generic;
+using static UnityEngine.Random;
 
 public class PowerUps : MonoBehaviour
 {
-    [SerializeField] string name;
-    private string powerup;
-    public Score score;
+    [SerializeField] List<Sprite> sprites;
+    string powerup;
+    private SpriteRenderer sr;
+    Score score;
 
+    private void Start() {
+        score = GameObject.Find("Score").GetComponent<Score>();
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = Instantiate(sprites[Random.Range(0, sprites.Count)]) as Sprite;
+    }
     private void OnTriggerEnter2D(Collider2D colission) {
         switch (name)
         {
